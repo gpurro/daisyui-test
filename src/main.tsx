9419223a9379833642be4727a3403c029a0a48ff
Guillermo Purro <gpurro@gmail.com>
2024-04-19 10:43:08 -0300

    changing router config approach

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './api/ReactQueryClient';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import Router from './router';
+import { Outlet, RouterProvider } from 'react-router-dom';
+import './index.css';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
-      <Router />
+      <Outlet />
     </QueryClientProvider>
   </React.StrictMode>
 );
